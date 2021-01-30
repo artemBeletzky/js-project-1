506fa84755595c9cb2a6954ade66c9cbf55f7a95
Artem Beletzky <belezky.artem@gmail.com>
2021-01-30 18:53:32 +0300

    added a function to check if a string is blanked, added a blank check to askNameAndGreet

--- src/cli-for-brain-even.js
+++ src/cli-for-brain-even.js
@@ -1,8 +1,16 @@
 import readlineSync from 'readline-sync';
 
+function checkIfBlank(str) {
+  return str.length === 0 || !str.trim();
+}
+
 function askNameAndGreet() {
   console.log('Welcome To The Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
+  let name = readlineSync.question("What's your name? ");
+  while (checkIfBlank(name)) {
+    console.log('In order to continue you need to enter your name.');
+    name = readlineSync.question("What's your name? ");
+  }
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   askQuestions(name);
@@ -13,19 +21,18 @@ function askNameAndGreet() {
 function askQuestions(name, inARow = 0) {
   if (inARow === 3) {
     console.log(`Congratulations, ${name}! You've won the game!`);
-    process.exit(); // askNameAndGreet gets invoked again, if not process.exit()
+    process.exit(); // askNameAndGreet gets invoked again, if I don't use process.exit()
   } else {
     const defineRandomNumber = Math.ceil(Math.random() * 100);
     const correctAnswer = defineRandomNumber % 2 === 0 ? 'yes' : 'no';
     console.log(`Question: ${defineRandomNumber}`);
     const userAnswer = readlineSync.question('Your answer: ');
     const result = correctAnswer === userAnswer;
-    const checkIfBlank = userAnswer.length === 0 || !userAnswer.trim();
     if (userAnswer === 'exit') {
       console.log('Bye-bye!');
       process.exit();
     }
-    if (checkIfBlank) {
+    if (checkIfBlank(userAnswer)) {
       console.log(`${name}, you forgot to enter the answer:(`);
       return askQuestions(name, inARow);
     }
@@ -34,7 +41,7 @@ function askQuestions(name, inARow = 0) {
       return askQuestions(name, inARow + 1);
     }
     console.log(
-      `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${name}!`
+      `'${userAnswer}' is the wrong answer ;(. The correct answer was '${correctAnswer}'. Let's try again, ${name}!`
     );
     return askQuestions(name, 0);
   }
