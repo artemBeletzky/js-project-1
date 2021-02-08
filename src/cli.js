04e910b4f9166563d7c6ee5bb6d718c7d78f9979
Artem Beletzky <belezky.artem@gmail.com>
2021-02-09 01:40:53 +0300

    edited the welcome phrase in the askNameAndGreet function

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 function askNameAndGreet() {
   console.log('Welcome To The Brain Games!');
-  const name = readlineSync.question("What's your name? ");
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
 }
