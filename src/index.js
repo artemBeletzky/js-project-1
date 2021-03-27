b4a238d39210463710b2b6a39b4eb68fd26966ed
Artem Beletzky <belezky.artem@gmail.com>
2021-03-27 23:38:03 +0300

    fix for eslint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const start = ({
   description,
-  getRound
+  getRound,
 }) => {
   console.log('Welcome To The Brain Games!');
   const name = readlineSync.question('May I have your name? ');
